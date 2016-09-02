067c06375a64c079014544cead8ad1da59b2f897
(ю:】 <gardentreex@gmail.com>
2016-09-02 20:21:19 +0900

    :+1: Make to redirect to before signin path

--- app/controllers/gofer/brain.rb
+++ app/controllers/gofer/brain.rb
@@ -2,6 +2,8 @@ module Gofer
   module Brain
     def require_login
       unless logged_in?
+        session[:path_before_signin] = request.fullpath
+
         redirect_to REQUEST_PATH
         return false
       end
--- app/controllers/gofer/sessions_controller.rb
+++ app/controllers/gofer/sessions_controller.rb
@@ -12,6 +12,8 @@ module Gofer
 
     private
       def after_logged_in_path
+        return session[:path_before_signin] if session[:path_before_signin].present?
+
         main_app.respond_to?(:root_path) ? main_app.root_path : "/"
       end
   end
