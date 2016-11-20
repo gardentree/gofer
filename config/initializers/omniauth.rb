4e4cdfe0cbf222b402242f04261ccd64ccd6cb9d
(ю:】 <gardentreex@gmail.com>
2016-11-20 11:33:12 +0900

    :+1: Change scope to environment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,7 @@ Gofer::REQUEST_PATH = "/gofer/sessions/new"
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],{
     name:           :google,
-    scope:          "email",
+    scope:          ENV["GOFER_SCOPE"],
     request_path:   Gofer::REQUEST_PATH,
     callback_path:  "/gofer/sessions/callback",
     redirect_uri:   ENV["GOOGLEAUTH_REDIRECT_URL"],
